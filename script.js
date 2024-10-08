148d7ccaf14ac1a41aa64679cd081df8dd6a1f5b
power <viswajiths697@gmail.com>
2024-10-08 15:08:21 +0530

    This is the 1st part of the my coding forportfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to add interactivity
+
+// Example: Alert message when the form is submitted
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent actual form submission for now
+    alert('Thank you for your message! I will get back to you soon.');
+});
+
+// You can add more interactivity like animations or effects below
